6f8a5aa456ba7c0d24f48dd80cc2707387fa4fba
Jazmine Stevenson <jazminestevenson@Jacquins-MacBook-Pro.local>
2022-06-03 12:03:08 -0400

    added comments

--- designs.js
+++ designs.js
@@ -1,9 +1,9 @@
-function makeGrid() {
+function makeGrid() { //function to create grid and select size input
     const height = $('#inputHeight').val();
     const width = $('#inputWidth').val();
     const $table = $('#pixelCanvas');
 
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i < height; i++) { //creates table
         const $row = $('<tr />');
         for (let j = 0; j < width; j++) {
             const $cell = '<td />';
@@ -13,7 +13,7 @@ function makeGrid() {
     }
 }
 
-function clearCanvas() {
+function clearCanvas() { //clear canvas function
     const $table = $('#pixelCanvas');
     $table.html('');
 }
@@ -24,13 +24,13 @@ function colorCell($cell, color) {
     $cell.css('background', chosenColor);
 }
 (function runApp() {
-    $('#sizePicker').submit(function(event) {
+    $('#sizePicker').submit(function(event) { //when size is submitted call makegrid()
         event.preventDefault();
         clearCanvas();
         makeGrid();
     });
 
-    $('#pixelCanvas').on('click', 'td', function() {
+    $('#pixelCanvas').on('click', 'td', function() { //when cell is clicked the color will change
         let $cell = $(this);
         colorCell($cell);
     });
